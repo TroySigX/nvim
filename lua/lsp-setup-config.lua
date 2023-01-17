3e66968cb25bbdc23d82b58121f402fcf6c52a9d
Tri Than <thantrongtri3@gmail.com>
2023-01-16 22:37:16 -0800

    add lsp definition and code action binding

--- lua/lsp-setup-config.lua
+++ lua/lsp-setup-config.lua
@@ -1,3 +1,7 @@
+local remap = vim.keymap.set
+remap('n', 'K', vim.lsp.buf.definition, { noremap = true, silent = true })
+remap('n', '<space>a', vim.lsp.buf.code_action, { noremap = true, silent = true })
+
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig')['clangd'].setup {
     capabilities = capabilities
