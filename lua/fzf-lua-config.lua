60607629cdd7ee23cd608b24d6d1880c8f25245f
Tri Than <thantrongtri3@gmail.com>
2023-01-16 22:54:13 -0800

    add live grep mapping to fzf

--- lua/fzf-lua-config.lua
+++ lua/fzf-lua-config.lua
@@ -1,10 +1,13 @@
 local map = vim.keymap.set
-map("n", "<F6>", ":lua require('fzf-lua').files()<CR>", {silent = true})
-map("i", "<F6>", "<Esc>:lua require('fzf-lua').files()<CR>", {silent = true})
-map("v", "<F6>", "<Esc>:lua require('fzf-lua').files()<CR>", {silent = true})
-map("t", "<F6>", "<C-\\><C-n>:lua require('fzf-lua').files()<CR>", {silent = true})
+local bufopts = { noremap = true, silent = true }
+map("n", "<F6>", ":lua require('fzf-lua').files()<CR>", bufopts)
+map("i", "<F6>", "<Esc>:lua require('fzf-lua').files()<CR>", bufopts)
+map("v", "<F6>", "<Esc>:lua require('fzf-lua').files()<CR>", bufopts)
+map("t", "<F6>", "<C-\\><C-n>:lua require('fzf-lua').files()<CR>", bufopts)
 
-map("n", "<F18>", ":lua require('fzf-lua').oldfiles()<CR>", {silent = true})
-map("i", "<F18>", "<Esc>:lua require('fzf-lua').oldfiles()<CR>", {silent = true})
-map("v", "<F18>", "<Esc>:lua require('fzf-lua').oldfiles()<CR>", {silent = true})
-map("t", "<F18>", "<C-\\><C-n>:lua require('fzf-lua').oldfiles()<CR>", {silent = true})
+map("n", "<F18>", ":lua require('fzf-lua').oldfiles()<CR>", bufopts)
+map("i", "<F18>", "<Esc>:lua require('fzf-lua').oldfiles()<CR>", bufopts)
+map("v", "<F18>", "<Esc>:lua require('fzf-lua').oldfiles()<CR>", bufopts)
+map("t", "<F18>", "<C-\\><C-n>:lua require('fzf-lua').oldfiles()<CR>", bufopts)
+
+map('n', '<space>gr', ":lua require('fzf-lua').live_grep()<CR>", bufopts)
