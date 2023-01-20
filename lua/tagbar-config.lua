314b69883d9bf46e25624d6b467046c94db7fe39
Tri Than <thantrongtri3@gmail.com>
2023-01-19 20:49:20 -0800

    set auto-focus in tagbar

--- lua/tagbar-config.lua
+++ lua/tagbar-config.lua
@@ -1,20 +1,7 @@
+vim.g.tagbar_autofocus = 1
 local map = vim.keymap.set
 local bufopts = { silent = true }
 
-map('n', '<F8>', function()
-    vim.cmd("TagbarToggle")
-    vim.cmd("wincmd l")
-end
-, { desc = "Toggle Tagbar", unpack(bufopts) })
-map('i', '<F8>', function()
-    vim.cmd.stopinsert()
-    vim.cmd("TagbarToggle")
-    vim.cmd("wincmd l")
-end
-, { desc = "Toggle Tagbar", unpack(bufopts) })
-map('v', '<F8>', function()
-    vim.cmd.stopinsert()
-    vim.cmd("TagbarToggle")
-    vim.cmd("wincmd l")
-end
-, { desc = "Toggle Tagbar", unpack(bufopts) })
+map('n', '<F8>', ":TagbarToggle<CR>", { desc = "Toggle Tagbar", unpack(bufopts) })
+map('i', '<F8>', "<ESC>:TagbarToggle<CR>", { desc = "Toggle Tagbar", unpack(bufopts) })
+map('v', '<F8>', "<ESC>:TagbarToggle<CR>", { desc = "Toggle Tagbar", unpack(bufopts) })
