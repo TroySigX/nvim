48e5226cb6c093d42293a4236b26ca016cb2675f
Tri Than <thantrongtri3@gmail.com>
2023-01-22 16:16:36 -0800

    save buffer when toggle terminal and add mapping desc

--- lua/toggleterm-nvim-config.lua
+++ lua/toggleterm-nvim-config.lua
@@ -1,9 +1,9 @@
 local map = vim.keymap.set
 local bufopts = { silent = true }
 
-map('n', '<F9>', ":ToggleTerm<CR>", { desc = 'Toggle Terminal', unpack(bufopts) })
-map('i', '<F9>', "<Esc>:ToggleTerm<CR>", { desc = 'Toggle Term', unpack(bufopts) })
-map('v', '<F9>', "<Esc>:ToggleTerm<CR>", { desc = 'Toggle Term', unpack(bufopts) })
+map('n', '<F9>', ":w<CR>:ToggleTerm<CR>", { desc = 'Toggle Terminal', unpack(bufopts) })
+map('i', '<F9>', "<Esc>:w<CR>:ToggleTerm<CR>", { desc = 'Toggle Term', unpack(bufopts) })
+map('v', '<F9>', "<Esc>:w<CR>:ToggleTerm<CR>", { desc = 'Toggle Term', unpack(bufopts) })
 map('t', '<F9>', "<C-\\><C-N>:ToggleTerm<CR>", { desc = 'Toggle Term', unpack(bufopts) })
 
 require("toggleterm").setup{
