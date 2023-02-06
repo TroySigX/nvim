08c31832c4904d0b4f9b6e69276ae9c4965ad88a
Tri Than <thantrongtri3@gmail.com>
2023-02-06 16:34:03 -0500

    add tsserver lsp

--- lua/lsp-setup-config.lua
+++ lua/lsp-setup-config.lua
@@ -6,6 +6,9 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig')['clangd'].setup {
     capabilities = capabilities
 }
+require('lspconfig')['tsserver'].setup {
+    capabilities = capabilities
+}
 -- require('lspconfig')['pyright'].setup {
 --     capabilities = capabilities
 -- }
