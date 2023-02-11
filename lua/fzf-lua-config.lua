82d89c661167e327710b1e3abca22a892113c0aa
Tri Than <thantrongtri3@gmail.com>
2023-02-10 21:14:34 -0500

    image viewer in fzf

--- lua/fzf-lua-config.lua
+++ lua/fzf-lua-config.lua
@@ -12,3 +12,19 @@ map('t', '<F18>', require('fzf-lua').oldfiles, { desc = 'Old files' })
 map('n', '<space>gr', require('fzf-lua').live_grep, { desc = 'Live [Gr]ep' })
 map('n', '<space>sw', require('fzf-lua').grep_cword, { desc = '[S]earch [W]ord' })
 map('n', '<space>km', require('fzf-lua').keymaps, { desc = '[K]ey[M]aps' })
+
+local img_previewer = { "viu", "-b" }
+
+require("fzf-lua").setup({
+    previewers = {
+        builtin = {
+            ueberzug_scaler = "cover",
+            extensions = {
+                ["gif"] = img_previewer,
+                ["png"] = img_previewer,
+                ["jpg"] = img_previewer,
+                ["jpeg"] = img_previewer,
+            },
+        },
+    },
+})
