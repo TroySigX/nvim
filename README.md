ddb14c2c6b5a3b2812520d6b131ca257990cb202
Tri Than <thantrongtri3@gmail.com>
2023-02-11 18:26:48 -0500

    update package installation for ueberzug

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 packer.nvim: yay -S nvim-packer-git <br />
 ripgrep: https://github.com/BurntSushi/ripgrep#installation <br />
 ctags: sudo pacman -S ctags <br />
+ueberzug: sudo pacman -S ueberzug <br />
--- lua/fzf-lua-config.lua
+++ lua/fzf-lua-config.lua
@@ -18,7 +18,6 @@ local img_previewer = { "ueberzug" }
 require("fzf-lua").setup({
     previewers = {
         builtin = {
-            ueberzug_scaler = "cover",
             extensions = {
                 ["gif"] = img_previewer,
                 ["png"] = img_previewer,
