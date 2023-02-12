8558b2f604d5ab0a1ac748c821befb857e407080
Tri Than <thantrongtri3@gmail.com>
2023-02-12 12:12:09 -0500

    add markdown live preview

--- lua/knap-config.lua
+++ lua/knap-config.lua
@@ -1,7 +1,14 @@
 local gknapsettings = {
-    texoutputext = "pdf",
-    textopdf = "pdflatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%",
-    textopdfviewerlaunch = "zathura %outputfile%",
-    textopdfviewerrefresh = "kill -HUP %pid%"
+    -- latex
+    texoutputext = 'pdf',
+    textopdf = 'pdflatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%',
+    textopdfviewerlaunch = 'zathura %outputfile%',
+    textopdfviewerrefresh = 'kill -HUP %pid%',
+
+    -- markdown
+    mdoutputext = 'pdf',
+    mdtopdf = 'pandoc %docroot% -o %outputfile%',
+    mdtopdfviewerlaunch = 'zathura %outputfile%',
+    mdtopdfviewerrefresh = 'kill -HUP %pid%',
 }
 vim.g.knap_settings = gknapsettings
