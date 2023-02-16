952a5c5b3268111a4064af5125ab56d05cacbb3d
Tri Than <thantrongtri3@gmail.com>
2023-02-15 23:10:58 -0500

    add mappings to open ChatGPT

--- lua/chatgpt-nvim-config.lua
+++ lua/chatgpt-nvim-config.lua
@@ -1,1 +1,5 @@
 require("chatgpt").setup()
+
+local map = vim.keymap.set
+map({ 'n' }, '<F11>', ':ChatGPT<CR>', { desc = 'Open ChatGPT' })
+map({ 'i', 'v' }, '<F11>', '<Esc>:ChatGPT<CR>', { desc = 'Open ChatGPT' })
