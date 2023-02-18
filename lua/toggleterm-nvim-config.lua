5440d1bcdd52dc6a06b8e13226df7513c0e1d79b
Tri Than <thantrongtri3@gmail.com>
2023-02-17 22:41:44 -0500

    fix: toggle terminal even when buffer is read only

--- lua/toggleterm-nvim-config.lua
+++ lua/toggleterm-nvim-config.lua
@@ -1,8 +1,22 @@
 local map = vim.keymap.set
 local bufopts = { silent = true }
 
-map('n', '<F9>', ':w<CR>:ToggleTerm<CR>', { desc = 'Toggle Terminal', unpack(bufopts) })
-map({ 'i', 'v' }, '<F9>', '<Esc>:w<CR>:ToggleTerm<CR>', { desc = 'Toggle Term', unpack(bufopts) })
+local toggleTerm = function()
+    local api = vim.api
+    -- check if buffer is modifiable
+    local buf = api.nvim_win_get_buf(0)
+    if not vim.bo[buf].readonly then
+        api.nvim_command('write')
+    end
+
+    api.nvim_command('ToggleTerm')
+end
+
+map('n', '<F9>', toggleTerm, { desc = 'Toggle Terminal', unpack(bufopts) })
+map({ 'i', 'v' }, '<F9>', function()
+    vim.cmd.stopinsert()
+    toggleTerm()
+end, { desc = 'Toggle Term', unpack(bufopts) })
 map('t', '<F9>', '<C-\\><C-N>:ToggleTerm<CR>', { desc = 'Toggle Term', unpack(bufopts) })
 
 require('toggleterm').setup{
