2d82a5184c97357b68684a8d5be8e8176f1451d2
Tri Than <thantrongtri3>
2023-02-24 10:06:54 -0500

    add luasnip mapping for jumping between placeholders

--- lua/luasnip-config.lua
+++ lua/luasnip-config.lua
@@ -1,1 +1,5 @@
 require('luasnip.loaders.from_snipmate').lazy_load({ paths = { '~/.config/nvim/snippets' } })
+
+local map = vim.keymap.set
+map('i', '<C-j>', function() require'luasnip'.jump(1) end, { desc = 'Go to next placeholder in snippet' })
+map('i', '<C-k>', function() require'luasnip'.jump(-1) end, { desc = 'Go to previous placeholder in snippet' })
