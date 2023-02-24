a226bc6323d9f42600d36735643712013c92a44e
Tri Than <thantrongtri3>
2023-02-24 11:14:18 -0500

    add mapping in s mode

--- lua/luasnip-config.lua
+++ lua/luasnip-config.lua
@@ -2,4 +2,6 @@ require('luasnip.loaders.from_snipmate').lazy_load({ paths = { '~/.config/nvim/s
 
 local map = vim.keymap.set
 map('i', '<C-j>', function() require'luasnip'.jump(1) end, { desc = 'Go to next placeholder in snippet' })
+map('s', '<C-j>', function() require'luasnip'.jump(1) end, { desc = 'Go to next placeholder in snippet' })
 map('i', '<C-k>', function() require'luasnip'.jump(-1) end, { desc = 'Go to previous placeholder in snippet' })
+map('s', '<C-k>', function() require'luasnip'.jump(-1) end, { desc = 'Go to previous placeholder in snippet' })
