a716175d6d2f33f72fa0aba852d79acc278e2a80
Tri Than <thantrongtri3>
2023-02-28 22:13:42 -0500

    add comment for mappings

--- lua/01-settings.lua
+++ lua/01-settings.lua
@@ -55,11 +55,12 @@ local signs = {
     { name = 'DiagnosticSignHint', text = '' },
     { name = 'DiagnosticSignInfo', text = '' },
 }
-
 for _, sign in ipairs(signs) do
     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 
 end
+
+-- diagnostic severity
 vim.diagnostic.config({
     virtual_text = false,
     signs = { severity = vim.diagnostic.severity.ERROR }
