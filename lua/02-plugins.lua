a9f9229779d3bd994fd8430e2d2f8e5ee53bdfa7
Tri Than <thantrongtri3>
2023-03-07 21:23:53 -0500

    replace ghosttext by firenvim

--- lua/02-plugins.lua
+++ lua/02-plugins.lua
@@ -269,8 +269,13 @@ require('lazy').setup({
     },
 
     {
-        'subnut/nvim-ghost.nvim',
-        build = ':call nvim_ghost#installer#install()'
+        'glacambre/firenvim',
+
+        cond = not not vim.g.started_by_firenvim,
+        build = function()
+            require("lazy").load({ plugins = "firenvim", wait = true })
+            vim.fn["firenvim#install"](0)
+        end
     },
 
     {
