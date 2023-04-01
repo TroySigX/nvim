1470e083f8d2fb040f8fedfd89b8e9cf2ceefead
Tri Than <thantrongtri3>
2023-04-01 12:20:40 -0400

    show macro recording status on statusline

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,5 +1,14 @@
 require('lualine').setup{
     options = {
         theme = 'onedark'
-    }
+    },
+    sections = {
+    lualine_x = {
+        {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+        }
+    },
+  },
 }
