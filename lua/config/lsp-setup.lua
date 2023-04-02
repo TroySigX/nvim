7b690ca0d1aa1d7df680596bf035b11dfba9bc04
Tri Than <thantrongtri3>
2023-04-02 11:09:40 -0400

    remove unneeded line

--- lua/config/lsp-setup.lua
+++ lua/config/lsp-setup.lua
@@ -5,7 +5,6 @@ map('n', '<space>ca', vim.lsp.buf.code_action, { desc = 'LSP [C]ode [A]ction' })
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lsp = require('lspconfig')
 local servers = { 'clangd', 'pyright', 'tsserver', 'lua_ls', 'texlab' }
--- local servers = lsp.util.available_servers()
 for _, ls in ipairs(servers) do
     lsp[ls].setup{
         capabilities = capabilities
