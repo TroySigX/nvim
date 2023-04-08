482e672955fdfeeaaa77f3fcc579d77d43bfcf8f
Tri Than <thantrongtri3>
2023-04-08 19:58:23 -0400

    fix keymap for symbols-outline

--- lua/config/symbols-outline.lua
+++ lua/config/symbols-outline.lua
@@ -1,4 +1,4 @@
 require("symbols-outline").setup()
 
 local map = vim.keymap.set
-map({ 'n', 'i', 'v' }, '<F11>', '<ESC>:SymbolsOutline<CR>', { desc = 'Toggle Tagbar', silent = true })
+map({ 'n', 'i', 'v' }, '<F5>', '<ESC>:SymbolsOutline<CR>', { desc = 'Toggle Tagbar', silent = true })
