eb7da21c562eb958fbfc1838453e0d3ec1078ba7
Tri Than <thantrongtri3@gmail.com>
2023-04-13 21:37:47 -0400

    add function to get list of installed LSPs

--- lua/config/lsp-setup.lua
+++ lua/config/lsp-setup.lua
@@ -2,9 +2,23 @@ local map = vim.keymap.set
 map('n', 'K', vim.lsp.buf.definition, { desc = 'LSP Definition' })
 map('n', '<space>ca', vim.lsp.buf.code_action, { desc = 'LSP [C]ode [A]ction' })
 
+function MasonLspPackages()
+	local registry = require('mason-registry')
+	local lsp = {}
+	for _, pkg in ipairs(registry.get_installed_package_names()) do
+		local pkg_info = registry.get_package(pkg)
+		for _, type in ipairs(pkg_info.spec.categories) do
+			if type == 'LSP' then
+				table.insert(lsp, pkg_info.name)
+			end
+		end
+	end
+	return lsp
+end
+
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lsp = require('lspconfig')
-local servers = { 'clangd', 'pyright', 'tsserver', 'lua_ls', 'texlab' }
+local servers = MasonLspPackages()
 for _, ls in ipairs(servers) do
   lsp[ls].setup{
     capabilities = capabilities
