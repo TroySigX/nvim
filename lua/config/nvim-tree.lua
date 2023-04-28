b7f9a4559a3360f32ddffbdfe8575f759aa4034b
Tri Than <thantrongtri3@gmail.com>
2023-04-27 23:36:11 -0400

    nvim-tree.mapping.list is deprecated, fix mappings accordingly

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,13 +1,12 @@
-local function open_tab_silent(node)
-  local api = require('nvim-tree.api')
+local map = vim.keymap.set
+local api = require('nvim-tree.api')
 
+local function open_tab_silent(node)
   api.node.open.tab(node)
   vim.cmd.tabprev()
 end
 
 local function open_tab(node)
-  local api = require('nvim-tree.api')
-
   api.node.open.tab(node)
   vim.cmd.tabprev()
   api.tree.close()
@@ -16,29 +15,26 @@ end
 
 local function goto_buffer_cwd()
   vim.cmd('wincmd l')
-  local api = require('nvim-tree.api')
   api.tree.close()
   vim.cmd('NvimTreeFindFile!')
 end
 
 require('nvim-tree').setup({
   disable_netrw = true,
-  view = {
-    mappings = {
-      list = {
-        { key = '<C-t>', action = 'open_tab', action_cb = open_tab },
-        { key = 'T', action = 'open_tab_silent', action_cb = open_tab_silent },
-        { key = 'CD', action = 'goto_cwd', action_cb = goto_buffer_cwd },
-      },
-    }
-  },
+
+  on_attach = function()
+    map('n', '<C-t>', open_tab)
+    map('n', 'T', open_tab_silent)
+    map('n', 'CD', goto_buffer_cwd)
+    map('n', '<Tab>', api.node.open.preview)
+    map('n', '<CR>', api.node.open.edit)
+  end,
+
   git = {
     ignore = false,
   },
 })
 
-local map = vim.keymap.set
-local api = require('nvim-tree.api')
 map({ 'n', 'i', 'v' }, '<F2>', function()
   vim.cmd.stopinsert()
   api.tree.toggle()
