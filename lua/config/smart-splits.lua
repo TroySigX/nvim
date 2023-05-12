a0ee3d1be8d7ab29f9d6b430ed50585e8343090b
Tri Than <thantrongtri3@gmail.com>
2023-05-11 23:34:52 -0400

    change keymap for smart-splits

--- lua/config/smart-splits.lua
+++ lua/config/smart-splits.lua
@@ -1,10 +1,10 @@
 require('smart-splits').setup({})
 
 local map = vim.keymap.set
-map('n', '<A-h>', require('smart-splits').resize_left, { desc = 'Resize buffer left' })
-map('n', '<A-j>', require('smart-splits').resize_down, { desc = 'Resize buffer down' })
-map('n', '<A-k>', require('smart-splits').resize_up, { desc = 'Resize buffer up' })
-map('n', '<A-l>', require('smart-splits').resize_right, { desc = 'Resize buffer right' })
+map('n', '<A-H>', require('smart-splits').resize_left, { desc = 'Resize buffer left' })
+map('n', '<A-J>', require('smart-splits').resize_down, { desc = 'Resize buffer down' })
+map('n', '<A-K>', require('smart-splits').resize_up, { desc = 'Resize buffer up' })
+map('n', '<A-L>', require('smart-splits').resize_right, { desc = 'Resize buffer right' })
 
 map('n', '<leader><leader>h', function() require('smart-splits').swap_buf_left({ move_cursor = true }) end, { desc = 'move buffer left' })
 map('n', '<leader><leader>j', function() require('smart-splits').swap_buf_down({ move_cursor = true }) end, { desc = 'move buffer down' })
