6dd0e83704e962d473159b97241d2ca4fcea10cb
Tri Than <thantrongtri3@gmail.com>
2023-05-12 17:26:38 -0400

    fix mappings for competitest

--- lua/config/competitest.lua
+++ lua/config/competitest.lua
@@ -2,7 +2,7 @@ require('competitest').setup()
 
 local map = vim.keymap.set
 map('n', '<space>tr', ':CompetiTestRun<CR>', { desc = '[T]est [R]un', noremap = true, silent = true })
-map('n', '<space>td', function()
+map('n', '<space>tdo', function()
   local type = vim.fn.input('What to download? [T]estcase / [C]ontest / [P]roblem: ')
   type = string.lower(type)
 
@@ -21,4 +21,7 @@ map('n', '<space>td', function()
   else
     print('Invalid input')
   end
-end, { desc = '[T]est [D]ownload', noremap = true, silent = true })
+end, { desc = '[T]est [D]ownload [O]ptions', noremap = true, silent = true })
+map('n', '<space>tdt', ':CompetiTestReceive testcases<CR>', { desc = '[T]est [D]ownload [T]estcases', noremap = true, silent = true })
+map('n', '<space>tdp', ':CompetiTestReceive problem<CR>', { desc = '[T]est [D]ownload [P]roblem', noremap = true, silent = true })
+map('n', '<space>tdc', ':CompetiTestReceive contest<CR>', { desc = '[T]est [D]ownload [C]ontest', noremap = true, silent = true })
