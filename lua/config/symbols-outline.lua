cdeb6eed27699e0f190153caad9b6c099f2f5ef4
Tri Than <thantrongtri3@gmail.com>
2023-05-16 00:29:12 -0400

    auto preview for symbols-outline

--- lua/config/symbols-outline.lua
+++ lua/config/symbols-outline.lua
@@ -1,4 +1,6 @@
-require('symbols-outline').setup()
+require('symbols-outline').setup({
+  auto_preview = true,
+})
 
 local map = vim.keymap.set
 map({ 'n', 'i', 'v' }, '<F5>', '<ESC>:SymbolsOutline<CR>', { desc = 'Toggle Tagbar', silent = true })
