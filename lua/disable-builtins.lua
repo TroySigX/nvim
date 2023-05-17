fa5f216bf9b1432c7f2b99d1f67fa7bcdf641bc0
Tri Than <thantrongtri3@gmail.com>
2023-05-17 09:41:22 -0400

    re-enable neccessary builtins

--- lua/disable-builtins.lua
+++ lua/disable-builtins.lua
@@ -2,21 +2,15 @@ local disabled_built_ins = {
   '2html_plugin',
   'getscript',
   'getscriptPlugin',
-  'gzip',
-  'logipat',
   'netrw',
   'netrwPlugin',
   'netrwSettings',
   'netrwFileHandlers',
-  'matchit',
-  'matchparen',
   'tar',
   'tarPlugin',
   'rrhelper',
   'vimball',
   'vimballPlugin',
-  'zip',
-  'zipPlugin',
 }
 
 for _, plugin in pairs(disabled_built_ins) do
