2ffd23927ef670889cdc79c05982f7eb16873535
Tri Than <thantrongtri3@gmail.com>
2023-05-18 12:18:06 -0400

    fix toggleterm: donot auto write and disable persistent mode

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,24 +1,3 @@
-local map = vim.keymap.set
-
-local toggleTerm = function()
-  local api = vim.api
-  local bufnr = api.nvim_get_current_buf()
-  local filename = api.nvim_buf_get_name(bufnr)
-  local modifiable = api.nvim_buf_get_option(bufnr, 'modifiable')
-
-  if filename ~= '' and modifiable then
-    api.nvim_command('write')
-  end
-
-  api.nvim_command('ToggleTerm')
-end
-
-map({ 'n', 'i', 'v' }, '<F9>', function()
-  vim.cmd.stopinsert()
-  toggleTerm()
-end, { desc = 'Toggle Term', silent = true })
-map('t', '<F9>', '<C-\\><C-N>:ToggleTerm<CR>', { desc = 'Toggle Term', silent = true })
-
 require('toggleterm').setup{
   direction = 'float',
   float_opts = {
@@ -27,4 +6,10 @@ require('toggleterm').setup{
     height = 35,
     winblend = 17,
   },
+  persist_mode = false,
 }
+
+local map = vim.keymap.set
+
+map({ 'n', 'i', 'v' }, '<F9>','<Esc>:ToggleTerm<CR>', { silent = true })
+map('t', '<F9>', '<C-\\><C-N>:ToggleTerm<CR>', { desc = 'Toggle Term', silent = true })
