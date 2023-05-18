019fef1195efc1ebf0cdb92ca62261d716eb3146
Tri Than <thantrongtri3@gmail.com>
2023-05-18 10:27:26 -0400

    reformat code

--- lua/config/lsp-setup.lua
+++ lua/config/lsp-setup.lua
@@ -21,7 +21,7 @@ require('lspsaga').setup({
 
 local map = vim.keymap.set
 
-map('n', 'K', ':Lspsaga peek_definition<CR>', { desc = 'LSP Definition', silent =true })
+map('n', 'K', ':Lspsaga peek_definition<CR>', { desc = 'LSP Definition', silent = true })
 map('n', '<space>ca', ':Lspsaga code_action<CR>', { desc = 'LSP [C]ode [A]ction', silent = true })
 map('n', '<leader>e', ':Lspsaga show_line_diagnostics<CR>', { desc = 'Show [E]rrors on line', silent = true })
 
@@ -33,5 +33,5 @@ map('n', '<C-j>', function()
   require('lspsaga.diagnostic'):goto_next({ severity = vim.diagnostic.severity.ERROR })
 end, { desc = 'Go To Next Error' })
 
-map('n', '<space>rnp', '::Lspsaga rename ++project<CR>', { desc = '[R]e[N]ame [P]roject', silent = true })
-map('n', '<space>rnf', '::Lspsaga rename<CR>', { desc = '[R]e[N]ame [F]ile', silent = true })
+map('n', '<space>rnp', ':Lspsaga rename ++project<CR>', { desc = '[R]e[N]ame [P]roject', silent = true })
+map('n', '<space>rnf', ':Lspsaga rename<CR>', { desc = '[R]e[N]ame [F]ile', silent = true })
