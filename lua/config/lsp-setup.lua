7f89900dda89134d82d85525b5f40f6171cf85b3
Tri Than <thantrongtri3@gmail.com>
2023-05-18 10:48:25 -0400

    remove duplication

--- lua/config/lsp-setup.lua
+++ lua/config/lsp-setup.lua
@@ -20,11 +20,15 @@ require('lspsaga').setup({
 })
 
 local map = vim.keymap.set
+local function opts(desc)
+  return { desc = desc, silent = true }
+end
 
-map('n', 'K', ':Lspsaga peek_definition<CR>', { desc = 'LSP Definition', silent = true })
-map('n', 'J', ':Lspsaga hover_doc<CR>', { desc = 'LSP Doc', silent = true })
-map('n', '<space>ca', ':Lspsaga code_action<CR>', { desc = 'LSP [C]ode [A]ction', silent = true })
-map('n', '<leader>e', ':Lspsaga show_line_diagnostics<CR>', { desc = 'Show [E]rrors on line', silent = true })
+
+map('n', 'K', ':Lspsaga peek_definition<CR>', opts('LSP Definition'))
+map('n', 'J', ':Lspsaga hover_doc<CR>', opts('LSP Doc'))
+map('n', '<space>ca', ':Lspsaga code_action<CR>', opts('LSP [C]ode [A]ction'))
+map('n', '<leader>e', ':Lspsaga show_line_diagnostics<CR>', opts('Show [E]rrors on line'))
 
 map('n', '<C-k>', function()
   require('lspsaga.diagnostic'):goto_prev({ severity = vim.diagnostic.severity.ERROR })
@@ -34,5 +38,5 @@ map('n', '<C-j>', function()
   require('lspsaga.diagnostic'):goto_next({ severity = vim.diagnostic.severity.ERROR })
 end, { desc = 'Go To Next Error' })
 
-map('n', '<space>rnp', ':Lspsaga rename ++project<CR>', { desc = '[R]e[N]ame [P]roject', silent = true })
-map('n', '<space>rnf', ':Lspsaga rename<CR>', { desc = '[R]e[N]ame [F]ile', silent = true })
+map('n', '<space>rnp', ':Lspsaga rename ++project<CR>', opts('[R]e[N]ame [P]roject'))
+map('n', '<space>rnf', ':Lspsaga rename<CR>', opts('[R]e[N]ame [F]ile'))
