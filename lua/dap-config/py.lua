9cff950bd53abbef57d653924a5201e983535d5a
Tri Than <thantrongtri3@gmail.com>
2023-06-22 20:26:29 +0700

    install debugpy using mason

--- lua/dap-config/py.lua
+++ lua/dap-config/py.lua
@@ -1,11 +1,11 @@
 local dap = require('dap')
 
-require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+require('dap-python').setup('~/.local/share/nvim/mason/packages/debugpy/venv/bin/python')
 
 table.insert(dap.configurations.python, {
   type = 'python',
   request = 'launch',
   name = 'Django',
   program = vim.fn.getcwd() .. '/manage.py',
-  args = {'runserver'},
+  args = { 'runserver' },
 })
--- setup/debuggers/py.sh
+++
@@ -1,5 +0,0 @@
-function install_debugpy() {
-    cd ~ && mkdir .virtualenvs && cd .virtualenvs
-    python -m venv debugpy
-    debugpy/bin/python -m pip install debugpy
-}
