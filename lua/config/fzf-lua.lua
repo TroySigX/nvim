737e6242a2e16bc90f5d1dabb716a532cec6c6c5
Tri Than <thantrongtri3@gmail.com>
2023-06-22 20:01:42 +0700

    disable keymap for fzf in terminal mode

--- lua/config/fzf-lua.lua
+++ lua/config/fzf-lua.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
-map({ 'n', 'i', 'v', 't' }, '<F6>', require('fzf-lua').files, { desc = 'Find files' })
+map({ 'n', 'i', 'v' }, '<F6>', require('fzf-lua').files, { desc = 'Find files' })
 
-map({'n', 'i', 'v', 't'}, '<F18>', require('fzf-lua').oldfiles, { desc = 'Old files' })
+map({ 'n', 'i', 'v' }, '<F18>', require('fzf-lua').oldfiles, { desc = 'Old files' })
 
 map('n', '<space>gr', require('fzf-lua').live_grep, { desc = 'Live [Gr]ep' })
 map('n', '<space>sw', require('fzf-lua').grep_cword, { desc = '[S]earch [W]ord' })
