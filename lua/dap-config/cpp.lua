5aab0bfdf85bd4999e40eeed8377afdd343e9236
Tri Than <thantrongtri3@gmail.com>
2023-06-22 20:02:06 +0700

    reformat code (fix tab size)

--- lua/dap-config/cpp.lua
+++ lua/dap-config/cpp.lua
@@ -1,24 +1,24 @@
 local dap = require('dap')
 
 dap.adapters.codelldb = {
-    type = 'server',
-    host = '127.0.0.1',
-    port = 13000,
-    executable = {
-        command = os.getenv('HOME') .. '/.local/share/nvim/mason/bin/codelldb',
-        args = {'--port', '13000'},
-    }
+  type = 'server',
+  host = '127.0.0.1',
+  port = 13000,
+  executable = {
+    command = os.getenv('HOME') .. '/.local/share/nvim/mason/bin/codelldb',
+    args = {'--port', '13000'},
+  }
 }
 
 dap.configurations.c = {
-    {
-        type = 'codelldb',
-        request = 'launch',
-        program = vim.fn.getcwd() .. '/a.out',
-        cwd = '${workspaceFolder}',
-        terminal = 'integrated',
-        stopOnEntry = false,
-    }
+  {
+    type = 'codelldb',
+    request = 'launch',
+    program = vim.fn.getcwd() .. '/a.out',
+    cwd = '${workspaceFolder}',
+    terminal = 'integrated',
+    stopOnEntry = false,
+  }
 }
 
 dap.configurations.cpp = dap.configurations.c
