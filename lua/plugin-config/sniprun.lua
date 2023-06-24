8b4f37e31f7c430fac55fed6f65899b734d467da
Tri Than <thantrongtri3@gmail.com>
2023-06-24 20:50:45 +0700

    add snip close keymap

--- lua/plugin-config/sniprun.lua
+++ lua/plugin-config/sniprun.lua
@@ -2,9 +2,11 @@ require('sniprun').setup({
   display = {
     'Classic',
     'VirtualTextOk',
-    'TempFloatingWindow'
+    'TempFloatingWindow',
+    'NvimNotify',
   }
 })
 
 local map = vim.keymap.set
-map({ 'n', 'v' }, '<space>sr', ':SnipRun', { desc = 'Trigger [S]nip [R]un' })
+map({ 'n', 'v' }, '<space>sr', ':SnipRun<CR>', { desc = 'Trigger [S]nip [R]un', silent = true })
+map('n', '<space>sc', ':SnipClose<CR>', { desc = '[S]nip [C]lose', silent = true })
