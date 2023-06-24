6379cd51a6153d8ff3b07c3837c34e004a9194a4
Tri Than <thantrongtri3@gmail.com>
2023-06-24 20:57:23 +0700

    disable auto-preview in tagbar

--- lua/plugin-config/tagbar.lua
+++ lua/plugin-config/tagbar.lua
@@ -3,7 +3,6 @@ vim.g.tagbar_sort = 0
 vim.g.tagbar_show_balloon = 0
 vim.g.tagbar_show_linenumbers = 1
 vim.g.tagbar_silent = 1
-vim.g.tagbar_autopreview = 1
 vim.g.tagbar_previewwin_pos = 'aboveleft'
 
 local map = vim.keymap.set
