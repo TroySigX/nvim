ec6e76e4c707d5d70acf14831610a814f8026adc
Tri Than <thantrongtri3@gmail.com>
2023-06-27 13:41:19 +0700

    fix keymap for lspsaga

--- lua/plugin-config/lsp-setup.lua
+++ lua/plugin-config/lsp-setup.lua
@@ -51,7 +51,7 @@ function M.keymaps()
 
     ['<space>'] = {
       ca = { ':Lspsaga code_action<CR>', 'LSP [C]ode [A]ction' },
-      r = {
+      rn = {
         name = '[R]e[N]ame',
         p = { ':Lspsaga rename ++project<CR>', '[R]e[N]ame [P]roject' },
         f = { ':Lspsaga rename<CR>', '[R]e[N]ame [F]ile' },
