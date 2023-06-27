5b948b56ab935ffca40cdc1cdea4ede90ed61ae7
Tri Than <thantrongtri3@gmail.com>
2023-06-27 11:47:46 +0700

    no need to declare function in keymap for muren

--- lua/plugin-config/muren.lua
+++ lua/plugin-config/muren.lua
@@ -6,7 +6,7 @@ end
 
 function M.keymaps()
   require('which-key').register({
-    ['<C-s>'] = { function() require('muren.api').toggle_ui({}) end, 'Toggle [S]ubstitute' },
+    ['<C-s>'] = { require('muren.api').toggle_ui, 'Toggle [S]ubstitute' },
   }, { mode = { 'n', 'v' } })
 end
 
