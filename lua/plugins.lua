361a9c05ce8ef63188768886f5891fc25d1dbfdc
Tri Than <thantrongtri3@gmail.com>
2023-06-30 18:42:24 +0700

    lazy load neoscroll

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -238,7 +238,6 @@ return {
 
   {
     'karb94/neoscroll.nvim',
-    event = 'VeryLazy',
     keys = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
     '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
     config = function()
