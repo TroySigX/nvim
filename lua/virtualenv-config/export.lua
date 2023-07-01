a33a2453f884604c92bd43ce7527711a4b8dc1a6
Tri Than <thantrongtri3@gmail.com>
2023-07-01 10:42:26 +0700

    fix: ensure venv variables are appended on a newline

--- lua/virtualenv-config/export.lua
+++ lua/virtualenv-config/export.lua
@@ -3,12 +3,31 @@
 local M = {}
 local path = require('lspconfig.util').path
 
+local function file_exists(file_path)
+  local f=io.open(file_path,'r')
+  if f~=nil then
+      f:close()
+      return true
+  else
+      return false
+  end
+end
+
 function M.export(venv_var)
   if next(venv_var) then
-    local file = io.open(path.join(vim.fn.getcwd(), '.nvim.lua'), 'a')
+    local file_path = path.join(vim.fn.getcwd(), '.nvim.lua')
+    local f_exist = file_exists(file_path)
+    local file = io.open(file_path, 'a')
     if file == nil then
       return
     end
+
+    -- appended text needs to be on a newline
+    if f_exist then
+      file:write('\n')
+    end
+
+    -- append venv variables to file
     for var_type, type_table in pairs(venv_var) do
       if next(type_table) then
         for var, var_value in pairs(type_table) do
