125f82e0353d2f4638b9fd404760a8adbc08e244
Tri Than <thantrongtri3@gmail.com>
2023-07-05 11:32:24 +0700

    format code

--- lua/plugin-config/workflow/overseer.lua
+++ lua/plugin-config/workflow/overseer.lua
@@ -4,12 +4,12 @@ function M.keymaps()
   return {
     { '<F5>', vim.cmd.OverseerRun, desc = 'Trigger Task Runner', mode = { 'n', 'i', 'v' } },
     { '<F17>', function ()
-      local overseer = require("overseer")
+      local overseer = require('overseer')
       local tasks = overseer.list_tasks({ recent_first = true })
       if vim.tbl_isempty(tasks) then
-        vim.notify("No tasks found", vim.log.levels.WARN)
+        vim.notify('No tasks found', vim.log.levels.WARN)
       else
-        overseer.run_action(tasks[1], "restart")
+        overseer.run_action(tasks[1], 'restart')
       end
     end, desc = 'Run Last Task', mode = { 'n', 'i', 'v' }},
     { '<space>os', vim.cmd.OverseerToggle, desc = '[O]ver[S]eer Toggle (toggle task runner window)', silent = true },
