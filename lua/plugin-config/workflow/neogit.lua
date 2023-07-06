d06abcaf2895335703ce89b62da72bdeb07b6ece
Tri Than <thantrongtri3@gmail.com>
2023-07-06 10:56:40 +0700

    remove default config in neogit setup

--- lua/plugin-config/workflow/neogit.lua
+++ lua/plugin-config/workflow/neogit.lua
@@ -1,53 +1,17 @@
 local M = {}
 
 function M.setup()
-  require('neogit').setup {
-    disable_signs = false,
-    disable_hint = false,
-    disable_context_highlighting = false,
+  require('neogit').setup({
     disable_commit_confirmation = true,
-    auto_refresh = true,
-    disable_builtin_notifications = false,
-    use_magit_keybindings = false,
     kind = 'tab',
-    console_timeout = 2000,
-    auto_show_console = true,
+    auto_show_console = false,
     popup = {
       kind = 'vsplit',
-    },
-    signs = {
-      -- { CLOSED, OPENED }
-      section = { '>', 'v' },
-      item = { '>', 'v' },
-      hunk = { '', '' },
     },
     integrations = {
       diffview = true
     },
-    sections = {
-      untracked = {
-        folded = false
-      },
-      unstaged = {
-        folded = false
-      },
-      staged = {
-        folded = false
-      },
-      stashes = {
-        folded = true
-      },
-      unpulled = {
-        folded = true
-      },
-      unmerged = {
-        folded = false
-      },
-      recent = {
-        folded = true
-      },
-    },
-  }
+  })
 end
 
 function M.keymaps()
