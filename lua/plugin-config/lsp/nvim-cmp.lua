76233b089922e64d26875cc17e93f95822dc9c7a
Tri Than <thantrongtri3@gmail.com>
2023-07-07 11:07:01 +0700

    add copilot icon to nvim-cmp

--- lua/plugin-config/lsp/nvim-cmp.lua
+++ lua/plugin-config/lsp/nvim-cmp.lua
@@ -14,6 +14,7 @@ function M.setup()
         mode = 'symbol_text', -- show only symbol annotations
         maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
         ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        symbol_map = { Copilot = '' },
       })
     },
     mapping = cmp.mapping.preset.insert({
