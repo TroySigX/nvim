162bbb2ba424f72c5f9575e108fa05df2ddccf0f
Tri Than <thantrongtri3@gmail.com>
2023-07-10 11:06:32 +0700

    format code

--- lua/plugin-config/dadbod.lua
+++ lua/plugin-config/dadbod.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-  vim.g.db_ui_save_location = vim.fn.stdpath 'config' .. require('plenary.path').path.sep .. 'db_ui'
+  vim.g.db_ui_save_location = vim.fn.stdpath('config') .. require('plenary.path').path.sep .. 'db_ui'
 end
 
 function M.keymaps()
