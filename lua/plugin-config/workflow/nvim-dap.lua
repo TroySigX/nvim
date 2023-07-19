b5a3aa50947481c0198b5ff5fb6bb9080182f44e
Tri Than <thantrongtri3@gmail.com>
2023-07-19 20:44:57 +0700

    format code

--- lua/plugin-config/workflow/nvim-dap.lua
+++ lua/plugin-config/workflow/nvim-dap.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-  require ('mason-nvim-dap').setup({
+  require('mason-nvim-dap').setup({
     ensure_installed = { 'codelldb', 'debugpy', 'js' },
     handlers = {
       function(config)
--- lua/plugin-config/workflow/overseer.lua
+++ lua/plugin-config/workflow/overseer.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.keymaps()
   return {
     { '<F5>', vim.cmd.OverseerRun, desc = 'Trigger Task Runner', mode = { 'n', 'i', 'v' } },
-    { '<F17>', function ()
+    { '<F17>', function()
       local overseer = require('overseer')
       local tasks = overseer.list_tasks({ recent_first = true })
       if vim.tbl_isempty(tasks) then
