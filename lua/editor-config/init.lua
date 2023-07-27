5a0c31ba27d1b1eaad2b185c64b854922d11bd7f
Tri Than <thantrongtri3@gmail.com>
2023-07-27 12:30:17 +0700

    editorconfig: auto-create config file if not exists

---
+++ lua/editor-config/init.lua
@@ -0,0 +1,49 @@
+local path = require('lspconfig.util').path
+
+local filename = '.editorconfig'
+local filepath = path.join(vim.fn.getcwd(), filename)
+
+local function tmp_file()
+  local tmp_path = path.join(vim.fn.getcwd(), filename .. '.tmp')
+
+  local function create()
+    local tmpf = io.open(tmp_path, 'w')
+    tmpf:write()
+    tmpf:close()
+  end
+
+  local function remove()
+    os.remove(tmp_path)
+  end
+
+  return {
+    create = create,
+    remove = remove,
+    path = tmp_path,
+  }
+end
+
+local function init_editorconfig()
+  local tmp = tmp_file()
+  tmp:create()
+
+  local config_exists = false
+
+  path.traverse_parents(tmp.path, function(dir)
+    if path.exists(path.join(dir, filename)) then
+      config_exists = true
+      return true
+    end
+  end)
+
+  if not config_exists then
+    local config_file = path.join(vim.fn.stdpath('config'), filename)
+
+    -- create symlink for editconfig
+    os.execute('ln -s ' .. config_file .. ' ' .. filepath)
+  end
+
+  tmp:remove()
+end
+
+vim.schedule(init_editorconfig)
