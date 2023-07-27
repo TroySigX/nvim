f022c48ac500789f23d0016892e9fc913bc587e8
Tri Than <thantrongtri3@gmail.com>
2023-07-27 14:45:59 +0700

    settings: set tab size to 2

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,9 +1,9 @@
 local opt = vim.opt
 opt.termguicolors = true
 opt.relativenumber = true
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
 opt.expandtab = true
 opt.ignorecase = true
 opt.smartcase = true
