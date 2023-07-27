54949dcf464058bc9450c2528d43a22297dd30df
Tri Than <thantrongtri3@gmail.com>
2023-07-27 11:23:50 +0700

    Revert "formatter-nvim: notify formatting"
    This reverts commit 617177ca603aa10a363ab66f19c6201006ead4e6.

--- lua/plugin-config/editing/formatter-nvim.lua
+++ lua/plugin-config/editing/formatter-nvim.lua
@@ -34,7 +34,6 @@ function M.setup()
   vim.api.nvim_create_autocmd('BufWritePost', {
     pattern = '*',
     callback = function()
-      vim.notify('Formatting file...')
       vim.cmd([[FormatWriteLock]])
     end,
   })
