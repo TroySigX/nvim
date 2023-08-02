4a631296d20684aa5a08d2340e6e976004666296
Tri Than <thantrongtri3@gmail.com>
2023-08-02 18:39:35 +0700

    prev commit

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ vim.loader.enable()
 
 require('lazy').setup({
   spec = {
+    { import = 'plugin-config.utility.plugins' },
     { import = 'plugin-config.lsp.plugins' },
     { import = 'plugin-config.editing.plugins' },
     { import = 'plugin-config.explorer.plugins' },
