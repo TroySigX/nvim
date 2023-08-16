dee12cde60394ed72a85594002b9356191143f96
Tri Than <thantrongtri3@gmail.com>
2023-08-16 14:47:17 +0700

    feat(knap): open pdf files using default pdf viewer

--- lua/plugin-config/workflow/knap.lua
+++ lua/plugin-config/workflow/knap.lua
@@ -5,13 +5,13 @@ function M.setup()
     -- latex
     texoutputext = 'pdf',
     textopdf = 'pdflatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%',
-    textopdfviewerlaunch = 'okular %outputfile%',
+    textopdfviewerlaunch = 'xdg-open %outputfile%',
     textopdfviewerrefresh = 'kill -HUP %pid%',
 
     -- markdown
     mdoutputext = 'pdf',
     mdtopdf = 'pandoc %docroot% -o %outputfile%',
-    mdtopdfviewerlaunch = 'okular %outputfile%',
+    mdtopdfviewerlaunch = 'xdg-open %outputfile%',
     mdtopdfviewerrefresh = 'kill -HUP %pid%',
   }
   vim.g.knap_settings = gknapsettings
