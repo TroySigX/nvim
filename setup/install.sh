a8704e2ee8267dd64bbf205cfde2ab076dfd5ad1
Tri Than <thantrongtri3@gmail.com>
2023-08-18 23:00:56 +0700

    refractor: format code

--- setup/install.sh
+++ setup/install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [[ $EUID -eq 0 ]]; then
-    echo "This script must not be run as root"
-    exit 1
+  echo "This script must not be run as root"
+  exit 1
 fi
 
 # Install system packages
 packages=(
-    "ripgrep"
-    "xclip"
-    "ctags"
+  "ripgrep"
+  "xclip"
+  "ctags"
 )
 
 for package in "${packages[@]}"; do
-    if ! pacman -Qi "$package" > /dev/null 2>&1; then
-        sudo pacman -S --noconfirm "$package"
-    fi
+  if ! pacman -Qi "$package" > /dev/null 2>&1; then
+    sudo pacman -S --noconfirm "$package"
+  fi
 done
