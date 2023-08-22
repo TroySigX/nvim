5318bb43963d66197f9596c3f74c171664b8b85f
Tri Than <thantrongtri3@gmail.com>
2023-08-22 15:57:20 +0700

    refractor: add comment to nvim-cmp setup

--- lua/plugin-config/lsp/nvim-cmp.lua
+++ lua/plugin-config/lsp/nvim-cmp.lua
@@ -64,6 +64,7 @@ function M.setup()
     },
   })
 
+  -- nvim-autopairs integration
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
   cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
 end
