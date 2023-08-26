10e29aeec51e4ef24f42b6abfd16d16a44dcd160
Tri Than <thantrongtri3@gmail.com>
2023-08-26 12:57:57 +0700

    lsp: merge lsp-plugin setup into mason-lspconfig

--- lua/plugin-config/lsp/lsp-setup.lua
+++ lua/plugin-config/lsp/lsp-setup.lua
@@ -1,10 +1,5 @@
 local M = {}
 
-local lang_based_plugins = {
-  rust_analyzer = 'rust-tools',
-  tsserver = 'typescript-tools',
-}
-
 function M.setup()
   -- setup mason
   require('mason').setup({
@@ -25,22 +20,33 @@ function M.setup()
 
   -- setup lsp
   local default_capabilities = require('cmp_nvim_lsp').default_capabilities()
+  local base_dir = 'plugin-config.lsp.'
+
+  local function config_path(server_name)
+    return base_dir .. server_name
+  end
 
   mason_lspcofig.setup_handlers({
     function(server_name)
+      require('lspconfig')[server_name].setup({
+        capabilities = default_capabilities,
+      })
+    end,
+
+    ['clangd'] = function()
       local server_capabilities = default_capabilities
+      server_capabilities.offsetEncoding = 'utf-8'
+      require('lspconfig')['clangd'].setup({
+        capabilities = server_capabilities,
+      })
+    end,
 
-      if server_name == 'clangd' then
-        server_capabilities.offsetEncoding = 'utf-8'
-      end
+    ['tsserver'] = function()
+      require(config_path('typescript-tools')).setup(default_capabilities)
+    end,
 
-      if not lang_based_plugins[server_name] then
-        require('lspconfig')[server_name].setup({
-          capabilities = server_capabilities,
-        })
-      else
-        require('plugin-config.lsp.' .. lang_based_plugins[server_name]).setup(server_capabilities)
-      end
+    ['rust_analyzer'] = function()
+      require(config_path('rust-tools')).setup(default_capabilities)
     end,
   })
 end
