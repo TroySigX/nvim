20546ec1d6be6facdfac3bb3162b53771d97713a
Tri Than <thantrongtri3@gmail.com>
2023-08-28 17:04:10 +0700

    lualine: indicate which lsp is attached

--- lua/plugin-config/gui/lualine.lua
+++ lua/plugin-config/gui/lualine.lua
@@ -9,9 +9,33 @@ function M.setup()
       lualine_x = {
         'overseer',
         {
+          -- Macro recording
           require('noice').api.status.mode.get,
           cond = require('noice').api.status.mode.has,
           color = { fg = 'ff9e64' },
+        },
+      },
+
+      lualine_c = {
+        {
+          -- LSP server name .
+          function()
+            local msg = 'No Active LSP'
+            local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+            local clients = vim.lsp.get_active_clients()
+            if next(clients) == nil then
+              return msg
+            end
+            for _, client in pairs(clients) do
+              local filetypes = client.config.filetypes
+              if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                return client.name
+              end
+            end
+            return msg
+          end,
+          icon = ' LSP:',
+          color = { fg = '#faa92f' },
         },
       },
     },
