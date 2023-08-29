6371560dddbbf3ae9444e9f3fc4be9b56391294c
Tri Than <thantrongtri3@gmail.com>
2023-08-29 11:24:40 +0700

    nvim-notify: add keymap to dismiss notifications

--- lua/plugin-config/gui/notify.lua
+++ lua/plugin-config/gui/notify.lua
@@ -1,5 +1,16 @@
 local M = {}
 
+function M.keymaps()
+  require('which-key').register({
+    ['<leader>nd'] = {
+      function()
+        require('notify').dismiss()
+      end,
+      '[N]otification [D]ismiss',
+    },
+  })
+end
+
 function M.setup()
   vim.notify = require('notify')
   vim.notify.setup({
