34c664aba5aeca1a572b711b4e66d7bfca676fb9
Tri Than <thantrongtri3@gmail.com>
2023-09-24 12:02:10 -0400

    typescript-tools: remove mason tsserver path
    this integration is now builtin

--- lua/plugin-config/lsp/typescript-tools.lua
+++ lua/plugin-config/lsp/typescript-tools.lua
@@ -1,14 +1,7 @@
 local M = {}
 
 function M.setup(capabilities)
-  local mason_registry = require('mason-registry')
-  local tsserver_path = mason_registry.get_package('typescript-language-server'):get_install_path()
-  local path = require('lspconfig.util').path
-
   require('typescript-tools').setup({
-    settings = {
-      tsserver_path = path.join(tsserver_path, 'node_modules', 'typescript', 'lib', 'tsserver.js'),
-    },
     capabilities = capabilities,
   })
 end
