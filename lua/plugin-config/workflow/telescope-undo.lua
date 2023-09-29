41ee01290efccb8a13c342a2bfc0f32b41b9acf0
Tri Than <thantrongtri3@gmail.com>
2023-09-28 21:56:28 -0400

    telescope-undo: manually setup keymaps

--- lua/plugin-config/workflow/telescope-undo.lua
+++ lua/plugin-config/workflow/telescope-undo.lua
@@ -14,7 +14,17 @@ function M.keymaps()
 end
 
 function M.setup()
-  require('telescope').load_extension('undo')
+  require('telescope').setup({
+    extensions = {
+      undo = {
+        mappings = {
+          i = {
+            ['<cr>'] = require('telescope-undo.actions').restore,
+          },
+        },
+      },
+    },
+  })
 end
 
 return M
