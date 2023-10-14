61525538e576c5ae74106e6cf5f20cb5fa2343a1
Tri Than <thantrongtri3@gmail.com>
2023-10-14 17:20:56 -0400

    dap-config: add 'program' property to codelldb

--- lua/dap-config/adapters/codelldb.lua
+++ lua/dap-config/adapters/codelldb.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local dap = require('dap')
+local path_sep = require('plenary.path').path.sep
 
 dap.adapters.codelldb = {
   type = 'server',
@@ -17,8 +18,18 @@ function M.adapter()
     {
       type = 'codelldb',
       request = 'launch',
-      -- program = vim.fn.getcwd() .. '/a.out',
       cwd = '${workspaceFolder}',
+      program = function()
+        local co = coroutine.running()
+        vim.ui.input({
+          prompt = 'Path to executable: ',
+          default = vim.fn.getcwd() .. path_sep,
+        }, function(input)
+          coroutine.resume(co, input)
+        end)
+
+        return coroutine.yield()
+      end,
       terminal = 'integrated',
       stopOnEntry = false,
     },
