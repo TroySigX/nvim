e7b6ea32d9d0d52e45d307cef4011c611c627717
Tri Than <thantrongtri3@gmail.com>
2023-10-18 13:57:12 -0400

    globals(path): remove traverse_up

--- lua/globals/path.lua
+++ lua/globals/path.lua
@@ -10,11 +10,6 @@ function M.traverse_parents(cwd, cb)
   lsp_path.traverse_parents(cwd, cb)
 end
 
-function M.traverse_up(cwd, cb)
-  cwd = M.join(cwd, '.tmp_file')
-  M.traverse_parents(cwd, cb)
-end
-
 function M.sep()
   return require('plenary.path').path.sep
 end
