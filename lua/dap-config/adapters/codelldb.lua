31ec985fd607eacc88b7880abcc41333a7a53be8
Tri Than <thantrongtri3@gmail.com>
2023-10-18 20:25:28 -0400

    dap-config: check executable for codelldb before saving to filetype

--- lua/dap-config/adapters/codelldb.lua
+++ lua/dap-config/adapters/codelldb.lua
@@ -1,8 +1,10 @@
 local M = {}
 
 local dap = require('dap')
-local path_sep = require('globals.path').sep()
-local file_to_executable = {}
+local path = require('globals.path')
+local path_sep = path.sep()
+local file_to_executable = {} -- save last executable for file
+local luarocks_installed = false -- only call nvim_rocks once
 
 dap.adapters.codelldb = {
   type = 'server',
@@ -13,6 +15,20 @@ dap.adapters.codelldb = {
     args = { '--port', '${port}' },
   },
 }
+
+local function is_executable(filepath)
+  -- checking if file exists
+  if not path.exists(filepath) then
+    return false
+  end
+
+  local lfs = require('lfs')
+  local attr = lfs.attributes(filepath)
+  if string.match(attr.permissions, 'x') then
+    return true
+  end
+  return false
+end
 
 function M.adapter()
   return {
@@ -30,7 +46,15 @@ function M.adapter()
         end)
 
         local executable = coroutine.yield()
-        file_to_executable[vim.fn.expand('%')] = executable
+        -- saving executable for file
+        if not luarocks_installed then
+          require('nvim_rocks').ensure_installed('luafilesystem')
+          luarocks_installed = true
+        end
+        if is_executable(executable) then
+          file_to_executable[vim.fn.expand('%')] = executable
+        end
+
         return executable
       end,
       terminal = 'integrated',
