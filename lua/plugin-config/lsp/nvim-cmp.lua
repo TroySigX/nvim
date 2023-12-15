6f5c0fb2b50f5fe2e9ed09e5ad2df644faaa1049
Tri Than <thantrongtri3@gmail.com>
2023-12-15 17:35:39 -0500

    nvim-cmp: remove completion menu

--- lua/plugin-config/lsp/nvim-cmp.lua
+++ lua/plugin-config/lsp/nvim-cmp.lua
@@ -12,6 +12,7 @@ function M.setup()
     formatting = {
       format = require('lspkind').cmp_format({
         mode = 'symbol_text',
+        menu = {},
         maxwidth = 50,
         ellipsis_char = '...',
         symbol_map = { Copilot = '' },
