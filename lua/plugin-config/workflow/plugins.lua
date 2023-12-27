e8e266b5f60d81d06fc4a061d6e4d3fd22aa6827
Tri Than <thantrongtri3@gmail.com>
2023-12-27 22:55:48 +0700

    plugins: remove nvim_rocks

--- lua/plugin-config/workflow/plugins.lua
+++ lua/plugin-config/workflow/plugins.lua
@@ -108,12 +108,6 @@ return {
   },
 
   {
-    'theHamsta/nvim_rocks',
-    lazy = true,
-    build = 'pip3 install --break-system-packages --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua',
-  },
-
-  {
     'linux-cultist/venv-selector.nvim',
     dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim' },
     keys = require(config_path('venv-selector')).keymaps(),
