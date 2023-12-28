9f9cfbb44a7ee5148a600e35fe6a55e65f21a28f
Tri Than <thantrongtri3@gmail.com>
2023-12-29 00:08:10 +0700

    refactor: cleanup

--- lua/dap-config/adapters/js-debug-adapter.lua
+++ lua/dap-config/adapters/js-debug-adapter.lua
@@ -32,7 +32,6 @@ function M.adapter()
       type = 'pwa-node',
       request = 'launch',
       name = 'Debug Jest Tests',
-      -- trace = true, -- include debugger info
       runtimeExecutable = 'node',
       runtimeArgs = {
         './node_modules/jest/bin/jest.js',
