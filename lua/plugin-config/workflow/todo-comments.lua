13cd60a5960c85d398e1480fa7422f7e11b7994a
Tri Than <thantrongtri3@gmail.com>
2024-01-28 11:21:33 +0700

    todo-comments: Use telescope to view todo list

--- lua/plugin-config/workflow/todo-comments.lua
+++ lua/plugin-config/workflow/todo-comments.lua
@@ -5,26 +5,12 @@ function M.setup()
 end
 
 function M.keymaps()
-  local function toggle_quickfix_list()
-    for _, win in pairs(vim.fn.getwininfo()) do
-      if win['quickfix'] == 1 then
-        vim.cmd.cclose()
-        return
-      end
-    end
-
-    vim.api.nvim_command('TodoQuickFix')
-  end
-
   require('which-key').register({
     ['<F3>'] = {
-      function()
-        vim.cmd.stopinsert()
-        toggle_quickfix_list()
-      end,
-      'Toggle TODO List',
+      vim.cmd.TodoTelescope,
+      'Open TODO List',
     },
-  }, { mode = { 'n', 'i', 'v' } })
+  }, { mode = { 'n', 'i', 'x' } })
 end
 
 return M
