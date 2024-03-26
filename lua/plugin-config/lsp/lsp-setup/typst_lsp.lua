4bbd7e16616b0d1de5a293543272d300410f8174
Tri Than <thantrongtri3@gmail.com>
2024-03-26 12:36:30 -0400

    lsp(typst): directly add settings to server

--- lua/plugin-config/lsp/lsp-setup/typst_lsp.lua
+++ lua/plugin-config/lsp/lsp-setup/typst_lsp.lua
@@ -2,11 +2,9 @@ local M = {}
 
 function M.setup(opts)
   local server_opts = vim.tbl_deep_extend('keep', opts, {})
-  server_opts = vim.tbl_deep_extend('force', server_opts, {
-    settings = {
-      exportPdf = 'never',
-    },
-  })
+  server_opts.settings = {
+    exportPdf = 'never',
+  }
   require('lspconfig')['typst_lsp'].setup(server_opts)
 end
 
