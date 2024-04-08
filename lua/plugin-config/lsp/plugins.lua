bb889380adb00f08a2b44d850490e0128f1c7211
Tri Than <thantrongtri3@gmail.com>
2024-04-08 14:01:07 -0400

    nvim-cmp: add path source

--- lua/plugin-config/lsp/plugins.lua
+++ lua/plugin-config/lsp/plugins.lua
@@ -9,6 +9,7 @@ return {
     'hrsh7th/nvim-cmp',
     event = { 'InsertEnter', 'CmdlineEnter' },
     dependencies = {
+      'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
