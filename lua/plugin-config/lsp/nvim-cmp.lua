5abb9086d64c808d66ff0d3a906957f419a5e625
Tri Than <thantrongtri3@gmail.com>
2024-05-20 23:08:31 +0700

    nvim-cmp: reorder comparators

--- lua/plugin-config/lsp/nvim-cmp.lua
+++ lua/plugin-config/lsp/nvim-cmp.lua
@@ -52,10 +52,10 @@ function M.setup()
 
     sorting = {
       comparators = {
+        cmp.config.compare.recently_used,
         cmp.config.compare.offset,
         cmp.config.compare.exact,
         cmp.config.compare.score,
-        cmp.config.compare.recently_used,
         require('cmp-under-comparator').under,
         cmp.config.compare.kind,
         cmp.config.compare.order,
