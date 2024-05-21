5e2e7628a4d30c1f8ab31306c05af929cf7b586c
Tri Than <thantrongtri3@gmail.com>
2024-05-21 19:01:23 +0700

    nvim-dap-python: don't init debug adapter if debugger not installed

--- lua/dap-config/preconfig-plugins/nvim-dap-python.lua
+++ lua/dap-config/preconfig-plugins/nvim-dap-python.lua
@@ -5,6 +5,10 @@ function M.setup()
   local path = require('utils.path')
 
   local mason_registry = require('mason-registry')
+  if not mason_registry.is_installed('debugpy') then
+    return
+  end
+
   local debugpy_path = mason_registry.get_package('debugpy'):get_install_path()
   require('dap-python').setup(path.join(debugpy_path, 'venv', 'bin', 'python'))
 
