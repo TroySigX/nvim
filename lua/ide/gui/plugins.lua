7f2014b17f4001e41c99a46c80e5a971b28285d2
Tri Than <thantrongtri3@gmail.com>
2024-06-28 13:20:06 +0700

    lualine: remove dependency

--- lua/ide/gui/plugins.lua
+++ lua/ide/gui/plugins.lua
@@ -14,7 +14,6 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
-      'folke/noice.nvim',
     },
     config = function()
       require(config_path('lualine')).setup()
