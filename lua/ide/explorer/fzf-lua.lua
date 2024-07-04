9413464a9d9b145ab2c2a897e9610f140f04f234
Tri Than <thantrongtri3@gmail.com>
2024-07-04 13:58:06 +0700

    fzf-lua: set hl group

--- lua/ide/explorer/fzf-lua.lua
+++ lua/ide/explorer/fzf-lua.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.setup()
   vim.api.nvim_set_hl(0, 'FzfLuaBorder', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'FzfLuaTitle', { link = 'Special' })
   require('fzf-lua').setup()
 end
 
