9d7d8cc394b3c41e3e6ae28d400dfa5149cd84bf
Tri Than <thantrongtri3@gmail.com>
2024-07-04 14:43:55 +0700

    venv, lualine: refactor

--- lua/ide/gui/lualine.lua
+++ lua/ide/gui/lualine.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local lsp_dir = require('utils.path').config_dir('lsp')()
+local workflow_dir = require('utils.path').config_dir('workflow')()
 
 function M.setup()
   require('lualine').setup({
@@ -73,27 +74,7 @@ function M.setup()
         {
           -- Venv name
           function()
-            local pyproject = vim.fn.findfile('pyproject.toml', vim.fn.getcwd() .. ';')
-            if pyproject == '' then
-              return ''
-            end
-
-            local venv = require('venv-selector').venv()
-            if venv then
-              local max_len = 30
-              local venv_parts = vim.fn.split(venv, '/')
-              local venv_name = venv_parts[#venv_parts]
-              local venv_name_len = venv_name:len()
-              venv_name = venv_name_len > max_len
-                  and venv_name:sub(1, max_len - 3 - 6) .. '...' .. venv_name:sub(
-                    venv_name_len - (6 - 1),
-                    venv_name_len
-                  )
-                or venv_name
-              return venv_name
-            else
-              return 'No Active Venv'
-            end
+            return require(workflow_dir).active_venv()
           end,
           icon = '',
           color = { fg = '#CDD6F4' },
--- lua/ide/workflow/init.lua
+++ lua/ide/workflow/init.lua
@@ -11,4 +11,25 @@ function M.setup_keymap()
   plugin_keymaps.setup()
 end
 
+function M.active_venv()
+  local pyproject = vim.fn.findfile('pyproject.toml', vim.fn.getcwd() .. ';')
+  if pyproject == '' then
+    return ''
+  end
+
+  local venv = require('venv-selector').venv()
+  if venv then
+    local max_len = 30
+    local venv_parts = vim.fn.split(venv, '/')
+    local venv_name = venv_parts[#venv_parts]
+    local venv_name_len = venv_name:len()
+    venv_name = venv_name_len > max_len
+        and venv_name:sub(1, max_len - 3 - 6) .. '...' .. venv_name:sub(venv_name_len - (6 - 1), venv_name_len)
+      or venv_name
+    return venv_name
+  else
+    return 'No Active Venv'
+  end
+end
+
 return M
