f5d82d89d7f312e37024137d622d53841c689cd7
Tri Than <thantrongtri3@gmail.com>
2024-07-13 22:43:41 +0700

    which-key: set options

--- lua/ide/others/which-key.lua
+++ lua/ide/others/which-key.lua
@@ -2,8 +2,12 @@ local M = {}
 
 function M.setup()
   require('which-key').setup({
+    preset = 'classic',
     icons = {
       rules = false,
+    },
+    win = {
+      no_overlap = false,
     },
     plugins = {
       registers = false,
