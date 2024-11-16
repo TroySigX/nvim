17799dbf42c5f557f47db9d1e158d6be4fda7dbc
Tri Than <thantrongtri3@gmail.com>
2024-11-16 12:23:34 -0500

    persistence: load cached venv when restoring session

--- lua/ide/workflow/persistence.lua
+++ lua/ide/workflow/persistence.lua
@@ -4,6 +4,12 @@ function M.keymaps()
   require('utils.keymaps').add_keymap({
     '<F7>',
     function()
+      -- venv selection should be run before session restore
+      local workflow_dir = require('utils.path').config_dir('workflow')()
+      if require(workflow_dir).active_venv() ~= '' then
+        vim.cmd.VenvSelectCached()
+      end
+
       require('persistence').load()
     end,
     'Load last session',
