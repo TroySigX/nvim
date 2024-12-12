024444036c883e2e69d80b72202ed87c03e3b5a3
Tri Than <thantrongtri3@gmail.com>
2024-12-12 16:03:22 -0500

    utils(path): replace lspconfig.path with vim.fs

--- lua/utils/path.lua
+++ lua/utils/path.lua
@@ -1,9 +1,5 @@
 local M = {}
 
-local lsp_path = function()
-  return require('lspconfig.util').path
-end
-
 --- path to plugin config file
 --- example: 'ide.lsp.treesitter'
 ---@param base_dir IDECapability | 'lsp.lsp-setup'
@@ -15,18 +11,14 @@ function M.config_dir(base_dir)
   end
 end
 
+-- join path
 function M.join(...)
-  return lsp_path().join({ ... })
-end
-
---- os path separator
-function M.sep()
-  return require('plenary.path').path.sep
+  return vim.fs.joinpath(...)
 end
 
 --- get working dir of file
-function M.path_dir(path)
-  return lsp_path().dirname(path)
+function M.dirname(path)
+  return vim.fs.dirname(path)
 end
 
 --- get working dir of buffer
@@ -34,7 +26,7 @@ function M.buf_dir(buf_id)
   local dir
   if
     not pcall(function() -- buffer is not file
-      dir = M.path_dir(vim.api.nvim_buf_get_name(buf_id))
+      dir = M.dirname(vim.api.nvim_buf_get_name(buf_id))
     end)
   then
     return nil
