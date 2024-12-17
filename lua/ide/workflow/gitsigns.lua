02337a3a600cfc581de6e910fdb6fd9921671f5d
Tri Than <thantrongtri3@gmail.com>
2024-12-17 18:08:07 -0500

    gitsigns: remove git_blame buffer

--- lua/ide/workflow/gitsigns.lua
+++ lua/ide/workflow/gitsigns.lua
@@ -8,13 +8,6 @@ function M.keymaps()
   local map = require('utils.keymaps').add_keymap
 
   map({
-    '<leader>gbb',
-    function()
-      require('gitsigns').blame()
-    end,
-    '[G]it [B]lame [B]uffer',
-  })
-  map({
     '<leader>gbl',
     function()
       require('gitsigns').blame_line()
