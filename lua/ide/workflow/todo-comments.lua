0a3c21758e02fe4c25a58360dffb966b73199feb
Tri Than <thantrongtri3@gmail.com>
2024-12-26 18:58:37 +0700

    todo-comments: ignore .gitignore and only search for todo and fixme

--- lua/ide/workflow/todo-comments.lua
+++ lua/ide/workflow/todo-comments.lua
@@ -1,13 +1,24 @@
 local M = {}
 
 function M.setup()
-  require('todo-comments').setup()
+  require('todo-comments').setup({
+    search = {
+      args = {
+        '--no-ignore',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+      },
+    },
+  })
 end
 
 function M.keymaps()
   require('utils.keymaps').add_keymap({
     '<F3>',
-    vim.cmd.TodoTelescope,
+    '<Esc>:TodoTelescope keywords=TODO,FIX<CR>',
     'Open TODO List',
     mode = { 'n', 'i', 'x' },
   })
