1d7318a74cef45dde091a52de3e1f43d4db71507
Tri Than <thantrongtri3@gmail.com>
2025-04-19 18:33:24 -0400

    settings: diagnostic signs
    adapt to nvim 0.11

--- lua/settings/after-plugin.lua
+++ lua/settings/after-plugin.lua
@@ -19,22 +19,18 @@ for _, mode in pairs(insertModes) do
   end, { desc = 'auto indent on newline in insert mode', expr = true })
 end
 
--- set signs
-local signs = {
-  { name = 'DiagnosticSignError', text = '' },
-  { name = 'DiagnosticSignWarn', text = '' },
-  { name = 'DiagnosticSignHint', text = '' },
-  { name = 'DiagnosticSignInfo', text = '' },
-}
-for _, sign in pairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
-end
-
--- diagnostic severity
+-- diagnostic
 vim.diagnostic.config({
-  virtual_text = false,
   severity_sort = true,
-  signs = { severity = { min = vim.diagnostic.severity.WARN } },
+  signs = {
+    severity = { min = vim.diagnostic.severity.WARN },
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.HINT] = '',
+      [vim.diagnostic.severity.INFO] = '',
+    },
+  },
 })
 
 -- restore cursor position
